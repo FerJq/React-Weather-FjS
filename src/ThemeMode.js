eb0cd55a9758a20ad7ba1106e45450f8f474b96f
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-04-01 17:10:19 -0400

    cleaning code

--- src/ThemeMode.js
+++ src/ThemeMode.js
@@ -2,19 +2,11 @@ import React from "react";
 import "./ThemeMode.css";
 
 export default function ChangeDayNight(props) {
-  const bodytheme = document.querySelector("body");
   let theme = props.theme;
   let date = props.gethour;
   const hour = date.getHours();
 
-  if ((hour < 6) | (hour > 18)) {
-    document.body.style.backgroundColor = "#1a191a";
-    bodytheme.classList.add("darktheme");
-  } else {
-    document.body.style.backgroundColor = "#bfe9ff";
-    bodytheme.classList.remove("darktheme");
-    props.valuefunction();
-  }
+
 
   return (
     <span className="ButtonForms">
